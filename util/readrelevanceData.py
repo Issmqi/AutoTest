fc29250da578cb8dec23a5d22a1be577bc257631
shimengqi <issmqi@outlook.com>
2020-04-02 22:57:30 +0800

    update apiSend

--- util/readrelevanceData.py
+++ util/readrelevanceData.py
@@ -17,22 +17,22 @@ def read_relevance_data(case_name,case_address):
     '''
 
 
-    response={}
-    full_data=ReadExcel(case_address).get_full_dict()
-    for i in full_data:
-        if i['CaseName']==case_name:
-            url=i['url']
-            method=
-            case_data=i
-            result=apiSend_param.send_request(url,method, parameter_type, parameter, cookie, header)
-            response=result[1]
-            break
-    if isinstance(response,dict):
-        print(response)
-        return response
-    else:
-        log.info('关联接口未返回dict响应')
-        return None
+    # response={}
+    # full_data=ReadExcel(case_address).get_full_dict()
+    # for i in full_data:
+    #     if i['CaseName']==case_name:
+    #         url=i['url']
+    #         method=
+    #         # case_data=i
+    #         # result=apiSend_param.send_request(url,method, parameter_type, parameter, cookie, header)
+    #         response=result[1]
+    #         break
+    # if isinstance(response,dict):
+    #     print(response)
+    #     return response
+    # else:
+    #     log.info('关联接口未返回dict响应')
+    #     return None
 
 
 
