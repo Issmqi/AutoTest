6d44d2e48b3dfc3a9c399b87f89725a0c7782e59
shimengqi <issmqi@outlook.com>
2020-04-23 17:54:57 +0800

    study

---
+++ business/study.py
@@ -0,0 +1,282 @@
+def add():
+    b = '12343'
+    a = '991232'
+    l = max(len(a), len(b))
+    a = a.zfill(l)
+    b = b.zfill(l)
+    res = [0] * l
+    flag = 0
+    # temp = "0" * max(l_a,l_b)
+    for i in range(l - 1, -1, -1):
+        temp = int(a[i]) + int(b[i]) + flag
+        if temp > 9:
+            flag = temp // 10
+            temp = temp % 10
+        res[i] = temp
+    if res[0] == 0:
+        res.insert(0, 1)
+    print(res)
+    return res
+
+
+# add()
+def findUnsortedSubarray(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    m = sorted(nums)
+    l, r = 0, len(nums) - 1
+    while l < r and nums[l] == m[l]:
+        l += 1
+    while l < r and nums[r] == m[r]:
+        r -= 1
+    return r - l + 1
+
+
+# findUnsortedSubarray([1,2,3,4])
+def nthUglyNumber(n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    res = [1]
+    p2 = 0
+    p3 = 0
+    p5 = 0
+    for i in range(n - 1):
+        # temp = min(res[p2] * 2, res[p3] * 3, res[p5] * 5)
+        # res.append(temp)
+        res.append(min(res[p2] * 2, res[p3] * 3, res[p5] * 5))
+        if res[-1] == res[p2] * 2:
+            p2 += 1
+        if res[-1] == res[p3] * 3:
+            p3 += 1
+        if res[-1] == res[p5] * 5:
+            p5 += 1
+    print(res[-1])
+    print(res)
+    return res[-1]
+
+# nthUglyNumber(30)
+
+
+def isUgly(num):
+    """
+    :type num: int
+    :rtype: bool
+    """
+    if num <1:
+        return False
+
+    while num%2==0:
+        num=num//2
+    while num%3==0:
+        num=num//3
+    while num%5==0:
+        num=num//5
+    return num==1
+
+# isUgly(27)
+def reverseString(s):
+    """
+    :type s: List[str]
+    :rtype: None Do not return anything, modify s in-place instead.
+
+    """
+    l,r=0,len(s)-1
+    while l<r:
+        s[l],s[r]=s[r],s[l]
+        l+=1
+        r-=1
+    print(s)
+    return s
+    # i=len(s)-1
+    # while i>=0:
+    #     s.append(s.pop(i))
+    #     i-=1
+    # return s
+# reverseString(["h","e","l","l","o"])
+def removeElement(nums, val):
+    """
+    :type nums: List[int]
+    :type val: int
+    :rtype: int
+    """
+    for i in range(len(nums) - 1, -1, -1):
+    # for i in range(len(nums)):
+        if nums[i]==val:
+            nums.pop(i)
+    return nums
+# removeElement([0,1,2,2,3,0,4,2],2)
+def findErrorNums(nums):
+    """
+    :type nums: List[int]
+    :rtype: List[int]
+    """
+    ss=sum(range(len(nums)+1))
+    re=sum(nums)-sum(set(nums))
+    S = sum(set(nums))
+    return [sum(nums) - S, len(nums) * (len(nums) + 1) // 2 - S]
+# findErrorNums([3,2,3,4,6,5])
+
+def shortestToChar(S,C):
+    cc=[i for i in range(len(S)) if C==S[i]]
+    print(cc)
+    re=[min(abs(x-i)  for i in cc) for x in range(len(S))]
+    print(re)
+    return re
+
+
+# S = "loveleetcode"
+# C = 'e'
+# shortestToChar(S,C)
+def uniqueOccurrences(arr):
+    """
+    :type arr: List[int]
+    :rtype: bool
+    """
+    num_dict = {}
+    for a in arr:
+        if a not in num_dict:
+            num_dict[a] = 1
+        else:
+            num_dict[a] += 1
+    nums_list = list(num_dict.values())
+    return len(nums_list) == len(set(nums_list))
+
+def isPalindrome(s):
+    s = s.lower()
+    ss = []
+    for i in s:
+        if i.isalnum():
+            ss.append(i)
+    return ss==ss[::-1]
+# isPalindrome(' abc ba ')
+def singleNumber(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    # return sum(set(nums))*2-sum(nums)
+    a = 0
+    for num in nums:
+        a = a ^ num
+    # print(a)
+    return a
+
+# singleNumber([2,1,2])
+def findDuplicates(nums):
+    res = []
+    for i in range(len(nums)):
+        loc = abs(nums[i]) - 1
+        if nums[loc] < 0:
+            res.append(loc + 1)
+        nums[loc] = -nums[loc]
+    return res
+# findDuplicates([4,3,2,7,8,2,3,1])
+
+
+# def findDuplicate(self, nums):
+#
+#     """
+#     :type nums: List[int]
+#     :rtype: int
+#     """
+#     # for i in range(len(nums)):
+#     #     loc = abs(nums[i]) - 1
+#     #     if nums[loc] < 0:
+#     #         return loc + 1
+#     #     nums[loc] = -nums[loc]
+#     for i in range(len(nums)):
+#         loc = abs(nums[i]) - 1
+#         if nums[loc] < 0:
+#             return loc + 1
+#         nums[loc] = -nums[loc]
+# findDuplicate([1,3,4,2,2])
+
+
+def sortArrayByParityII(A):
+    i ,j= 0,1
+    while i < len(A) - 1:
+        if A[i] % 2 == 0:
+            i += 2
+        elif A[j] % 2 == 1:
+            j += 2
+        else:
+            temp=A[i]
+            A[i]=A[j]
+            A[j]=temp
+    print(A)
+    return A
+
+# sortArrayByParityII([4,2,5,7])
+def addDigits(num):
+    # if num > 9:
+    #     num = num % 9
+    #     if num == 0:
+    #         return 9
+    # return num
+    return (num-1)%9+1
+
+
+
+def findDuplicate(nums):
+
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    # for i in range(len(nums)):
+    #     loc = abs(nums[i]) - 1
+    #     if nums[loc] < 0:
+    #         return loc + 1
+    #     nums[loc] = -nums[loc]
+
+
+    # left = 1
+    # right = len(nums)
+    # while left < right:
+    #     mid = int(left + (right - left) / 2)
+    #     cnt = 0
+    #     for num in nums:
+    #         if num <= mid:
+    #             cnt += 1
+    #     if cnt <= mid:
+    #         left = mid + 1
+    #     else:
+    #         right = mid
+    # return right
+
+    s=0
+    f=0
+    while True:
+        s=nums[s]
+        f=nums[nums[f]]
+        if s==f:
+            f=0
+
+            while nums[s]!=nums[f]:
+                f=nums[f]
+                s=nums[s]
+            return nums[s]
+
+
+# findDuplicate([1,3,4,2,2])
+def plusPne(digits):
+    flag = 1
+    for i in range(len(digits) - 1, -1, -1):
+        temp = digits[i]  + flag
+        if temp > 9:
+            flag = 1
+            digits[i] = temp % 10
+        else:
+            flag=0
+            digits[i] = temp
+    if digits[0] == 0:
+        digits.insert(0, 1)
+    return digits
+# plusPne([1,2,4])
+a=list(map(str, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+print(a)
+print(''.join(a))
--- util/httpClient.py
+++ util/httpClient.py
@@ -74,7 +74,6 @@ class HttpClient():
 
         return result
 
-
     def api_send(self, method, url, param, cookie, header=None, param_type=None):
         '''
 
@@ -142,15 +141,13 @@ class HttpClient():
             log.war('ERROR')
             log.error(e)
 
-
-    def ini_parameter(self,dependCase, relevance, parameter):
+    def ini_parameter(self, dependCase, relevance, parameter):
         '''
         初始化有依赖关系的参数
-        :param dependCase:
-        :param relevance:
+        :param dependCase:dict
+        :param relevance:dict
         :param parameter: str
-        :param case_address:
-        :return:
+        :return: parameter: str
         '''
         relevance_response = self.read_relevance_data(dependCase)
         if relevance_response:  # 关联接口返回不为空
@@ -168,8 +165,7 @@ class HttpClient():
             log.info('关联接口响应为空！')
             return parameter
 
-
-    def read_relevance_data(self,case_name):
+    def read_relevance_data(self, case_name):
 
         '''
         读取关联case请求参数
@@ -284,8 +280,8 @@ data = {'CaseId': 2.0, 'CaseName': 'get_allocation_bill_detail', 'APIName': '查
         'ExpectedData': '{"id":"0cVwUbuRXnzAhyFKRyaq","dr":0,"ts":1585301477000,"creator":"smq","creationTime":1585301477000,"modifier":null,"modifiedTime":null,"persistStatus":"nrm","promptMessage":null,"state":0,"approver":null,"approveTime":null,"approveOpinion":null,"sycnNCStatus":null,"sycnOutStatus":null,"pkOrgId":"abd7cf79-511d-4307-9bbd-d288b18d0ef9","pkOrgCode":"1210","pkOrgName":"西安喜马拉雅网络科技有限公司","pkOrgInId":"abd7cf79-511d-4307-9bbd-d288b18d0ef9","pkOrgInCode":"1210","pkOrgInName":"西安喜马拉雅网络科技有限公司","code":"DBO20200327000005","billDate":1585238400000,"billType":"Allocation","billTranTypeId":"Allocation","billTranTypeCode":"Allocation","billTranTypeName":"调拨单","outStorageId":"1001ZZ100000000DPAP4","outStorageCode":"test030201","outStorageName":"测试仓库030201","outIfSlotManage":null,"inStorageId":"1001ZZ100000000DPAP6","inStorageCode":"test030202","inStorageName":"test030202","inIfSlotManage":null,"outBizPersonId":"0mWG6nOSCxjsi1zoKEYs","outBizPersonCode":"007","outBizPersonName":"师孟奇","inBizPersonId":"0wsjdHVUBFlca3pyH155","inBizPersonCode":"2539","inBizPersonName":"唐林","outDeptId":"ae96b9ed-82dc-4aa7-bf97-cdd18a18c1a9","outDeptCode":"01010102","outDeptName":"城市经理","inDeptId":"02f265b0-aca6-4364-bd1b-50de07ef9482","inDeptCode":"02","inDeptName":"中台&业务支撑","planSendDate":null,"planArriveDate":null,"currencyId":"CURRENCY-01","currencyCode":"RMB","currencyName":"人民币","totalFactOutNum":null,"totalFactInNum":null,"billStatusId":null,"billStatusCode":null,"billStatusName":null,"transferStatusId":"0s21f51f-4f42-4100-dkd0-3254fbq33e6k","transferStatusCode":"1","transferStatusName":"待处理","stockBillBelong":null,"customerId":null,"customerName":null,"customerCode":null,"isClose":0,"closer":null,"closeDate":null,"closeReason":null,"remark":"自动化测试新增调拨单","transferBillItems":[{"id":"0nDo9IaykpX0aSOxITRr","dr":0,"ts":1585301487000,"creator":"smq","creationTime":1585301487000,"modifier":null,"modifiedTime":null,"persistStatus":"nrm","promptMessage":null,"rowNum":10,"transferBillId":"0cVwUbuRXnzAhyFKRyaq","goodsId":"03e77ae0-469d-4d8a-ba34-733c2ada3749","goodsCode":"301020000049","goodsName":"小雅AI音箱旗舰版_石墨绿","goodsFullName":null,"goodsBasicUnitName":"个","goodsAssistUnitName":"个","goodsConversionRate":1.000000,"enableBatchNumberManage":null,"productId":null,"productLineId":null,"isOptional":null,"unitId":"UNIT-12","unitCode":"EA","unitName":"个","transferNum":5.00000000,"onwayNum":null,"totalOutNum":null,"totalInNum":null,"unitPrice":null,"amountMoney":0E-8,"remark":null,"batchNumId":null,"batchNumCode":null,"batchNumName":null,"goodsPositionId":null,"goodsPositionCode":null,"goodsPositionName":null,"receiverAddress":null,"provinceId":null,"provinceCode":null,"provinceName":null,"cityId":null,"cityCode":null,"cityName":null,"countyId":null,"countyCode":null,"countyName":null,"townId":null,"townCode":null,"townName":null,"detailAddr":null,"receiver":null,"receiverPhone":null,"receiverPhoneSpare":null,"isClose":0,"sourceId":null,"sourceLineNum":null,"sourceType":null,"ext01":null,"ext02":null,"ext03":null,"ext04":null,"ext05":null,"ext06":null,"ext07":null,"ext08":null,"ext09":null,"ext10":null,"ext11":null,"ext12":null,"ext13":null,"ext14":null,"ext15":null,"goodsVersion":"1","goodsSelection":null,"isMotherPiece":null,"customerId":null,"customerCode":null,"customerName":null,"supplierId":null,"supplierCode":null,"supplierName":null,"projectId":null,"projectCode":null,"projectName":null,"batchCodeId":null,"batchCodeCode":null,"stockStateId":null,"stockStateCode":null,"stockStateName":null,"enableBatchNoManage":null,"enableInvStatusManage":null,"originalGoodsId":null,"goodsSelectionDescription":null,"outStorageId":"1001ZZ100000000DPAP4","outStorageCode":"test030201","outStorageName":"测试仓库030201","outIfSlotManage":null,"inStorageId":"1001ZZ100000000DPAP6","inStorageCode":"test030202","inStorageName":"test030202","inIfSlotManage":null,"outPositionId":null,"outPositionCode":null,"outPositionName":null}],"transferBillItemBoms":[{"id":"06NAx8X65U6lDo4qq2oJ","dr":0,"ts":1585301487000,"creator":"smq","creationTime":1585301487000,"modifier":null,"modifiedTime":null,"persistStatus":"nrm","promptMessage":null,"rowNum":"10","goodsId":"03e77ae0-469d-4d8a-ba34-733c2ada3749","goodsCode":"301020000049","goodsName":"小雅AI音箱旗舰版_石墨绿","goodsFullName":null,"goodsBasicUnitName":"个","goodsAssistUnitName":"个","goodsConversionRate":1.000000,"enableBatchNumberManage":null,"productId":null,"productLineId":null,"unitId":"UNIT-12","unitCode":"EA","unitName":"个","transferNum":5.00000000,"onwayNum":null,"totalOutNum":null,"totalInNum":null,"unitPrice":null,"amountMoney":0E-8,"remark":null,"batchNumId":null,"batchNumCode":null,"batchNumName":null,"goodsPositionId":null,"goodsPositionCode":null,"goodsPositionName":null,"receiverAddress":null,"provinceId":null,"provinceCode":null,"provinceName":null,"cityId":null,"cityCode":null,"cityName":null,"countyId":null,"countyCode":null,"countyName":null,"townId":null,"townCode":null,"townName":null,"detailAddr":null,"receiver":null,"receiverPhone":null,"receiverPhoneSpare":null,"isClose":0,"sourceId":null,"sourceLineNum":null,"sourceType":null,"ext01":null,"ext02":null,"ext03":null,"ext04":null,"ext05":null,"ext06":null,"ext07":null,"ext08":null,"ext09":null,"ext10":null,"ext11":null,"ext12":null,"ext13":null,"ext14":null,"ext15":null,"goodsVersion":"1","goodsSelection":null,"customerId":null,"customerCode":null,"customerName":null,"supplierId":null,"supplierCode":null,"supplierName":null,"projectId":null,"projectCode":null,"projectName":null,"batchCodeId":null,"batchCodeCode":null,"stockStateId":null,"stockStateCode":null,"stockStateName":null,"originalGoodsId":null,"goodsSelectionDescription":null,"itemId":"0nDo9IaykpX0aSOxITRr","billId":"0cVwUbuRXnzAhyFKRyaq","parentGoodsId":"03e77ae0-469d-4d8a-ba34-733c2ada3749","parentGoodsCode":"301020000049","parentGoodsName":"小雅AI音箱旗舰版_石墨绿","parentGoodsdisplayName":null,"parentRowNum":"10","childGoodsQty":null,"firstBillBomCode":null,"srcBillBomCode":null,"outStorageId":"1001ZZ100000000DPAP4","outStorageCode":"test030201","outStorageName":"测试仓库030201","outIfSlotManage":null,"inStorageId":"1001ZZ100000000DPAP6","inStorageCode":"test030202","inStorageName":"test030202","inIfSlotManage":null,"outPositionId":null,"outPositionCode":null,"outPositionName":null}],"ext01":null,"ext02":null,"ext03":null,"ext04":null,"ext05":null,"ext06":null,"ext07":null,"ext08":null,"ext09":null,"ext10":null,"ext11":null,"ext12":null,"ext13":null,"ext14":null,"ext15":null,"isDistribution":null,"isReturned":null}',
         'User': 'Manager', 'DependCase': 'create_allocation_bill', 'RelevanceList': '{"id":"id"}', 'IsDepend': ''}
 
-full_dict=ReadExcel(address).get_full_dict()
+full_dict = ReadExcel(address).get_full_dict()
 
 if __name__ == '__main__':
-    h=HttpClient(full_dict)
+    h = HttpClient(full_dict)
     h.send_requests(data)
